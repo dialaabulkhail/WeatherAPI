50c6fc04bf4963e41e04c06cb8904a4e39294576
diala <diala.sh.98@gmail.com>
2022-10-23 09:30:51 +0300

    deploy

--- pages/Parent.js
+++ pages/Parent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Weather from "./Weather";
+import Weather from "./components/Weather";
 import Image from "next/image";
 import { BsSearch } from "react-icons/bs";
 
